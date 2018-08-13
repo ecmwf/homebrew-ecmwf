941f96f371e19ad66a90eb48b7e63d1ac4ce5109
Tiago Quintino <tiago.quintino@ecmwf.int>
2018-08-13 15:45:43 +0100

    eckit: use ecbuild

--- eckit.rb
+++ eckit.rb
@@ -1,22 +1,18 @@
 class Eckit < Formula
   desc "ECMWF cross-platform c++ toolkit"
   homepage "https://github.com/ecmwf/eckit"
-  url "https://github.com/ecmwf/eckit/releases/download/0.22.0/eckit-0.22.0-Source.tar.gz"
-  sha256 "18cde98d62629a78355c40dfa52d69972c85b409fa482ac4efc4e383ebcd18f0"
-
-  # bottle do
-  #   cellar :any
-  #   sha256 "397a555e36bdad548c7121c3962d518dc304dc9d033fcf67c4ee4428a460285a" => :high_sierra
-  # end
+  url "https://github.com/ecmwf/eckit/archive/0.22.0.tar.gz"
+  sha256 "f6bf8f314e771077474242b0105d65a0e91509946c89b5509d2c284ce149fa1c"
 
   depends_on "cmake" => :build
+  depends_on "ecbuild" => :build
   # depends_on "eigen" => :recommended # currently fails to build -- internactive make works, non-interactive fails
   depends_on "armadillo" => :optional
   depends_on "viennacl" => :optional
 
   def install
     mkdir "build" do
-      system "cmake", "..", "-DENABLE_MPI=OFF", "-DENABLE_EIGEN=OFF", *std_cmake_args
+      system "ecbuild", "..", "-DENABLE_MPI=OFF", "-DENABLE_EIGEN=OFF", *std_cmake_args
       system "make", "install"
     end
   end
