4058d94c121c5f2114839e9ab927ff1fbabbb246
Tiago Quintino <tiago.quintino@ecmwf.int>
2018-08-31 17:23:55 +0100

    eckit 0.23.0 update

--- eckit.rb
+++ eckit.rb
@@ -1,8 +1,8 @@
 class Eckit < Formula
   desc "ECMWF cross-platform c++ toolkit"
   homepage "https://github.com/ecmwf/eckit"
-  url "https://github.com/ecmwf/eckit/archive/0.22.0.tar.gz"
-  sha256 "f6bf8f314e771077474242b0105d65a0e91509946c89b5509d2c284ce149fa1c"
+  url "https://github.com/ecmwf/eckit/archive/0.23.0.tar.gz"
+  sha256 "4ebc6e8d27f6107db3b18bb50ebf2d78dbc6f11de643c0b7979203b062f451e4"
 
   depends_on "cmake" => :build
   depends_on "ecbuild" => :build
@@ -18,6 +18,6 @@ class Eckit < Formula
   end
 
   test do
-    assert_match "0.22.0", shell_output("#{bin}/eckit-version").strip
+    assert_match "0.23.0", shell_output("#{bin}/eckit-version").strip
   end
 end
