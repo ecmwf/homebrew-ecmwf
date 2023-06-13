296ae9b75bb028962d2f5f122341ef7e3d3bcc9b
Dusan Figala <dusanfigala@protonmail.com>
2023-06-13 15:38:25 +0200

    eckit 1.23.1

--- Formula/eckit.rb
+++ Formula/eckit.rb
@@ -1,23 +1,45 @@
 class Eckit < Formula
   desc "ECMWF cross-platform c++ toolkit"
   homepage "https://github.com/ecmwf/eckit"
-  url "https://github.com/ecmwf/eckit/archive/0.23.0.tar.gz"
-  sha256 "4ebc6e8d27f6107db3b18bb50ebf2d78dbc6f11de643c0b7979203b062f451e4"
+  url "https://github.com/ecmwf/eckit/archive/refs/tags/1.23.1.tar.gz"
+  sha256 "cd3c4b7a3a2de0f4a59f00f7bab3178dd59c0e27900d48eaeb357975e8ce2f15"
+  license "Apache-2.0"
+
+  livecheck do
+    url "https://github.com/ecmwf/eckit/tags"
+    regex(/^v?(\\d(?:\\.\\d+)+)$/i)
+  end
 
   depends_on "cmake" => :build
   depends_on "ecbuild" => :build
-  # depends_on "eigen" => :recommended # currently fails to build -- internactive make works, non-interactive fails
+  depends_on "eigen" => :recommended
   depends_on "armadillo" => :optional
   depends_on "viennacl" => :optional
 
   def install
     mkdir "build" do
-      system "ecbuild", "..", "-DENABLE_MPI=OFF", "-DENABLE_EIGEN=OFF", *std_cmake_args
+      system "ecbuild", "..", "-DENABLE_MPI=OFF", *std_cmake_args
       system "make", "install"
     end
+
+    shim_references = [
+      lib/"pkgconfig/eckit_mpi.pc",
+      lib/"pkgconfig/eckit_cmd.pc",
+      lib/"pkgconfig/eckit_test_value_custom_params.pc",
+      lib/"pkgconfig/eckit_option.pc",
+      lib/"pkgconfig/eckit_maths.pc",
+      lib/"pkgconfig/eckit_web.pc",
+      lib/"pkgconfig/eckit_sql.pc",
+      lib/"pkgconfig/eckit.pc",
+      lib/"pkgconfig/eckit_linalg.pc",
+      lib/"pkgconfig/eckit_geometry.pc",
+      include/"eckit/eckit_ecbuild_config.h",
+    ]
+    inreplace shim_references, Superenv.shims_path/ENV.cxx, ENV.cxx
+    inreplace shim_references, Superenv.shims_path/ENV.cc, ENV.cc
   end
 
   test do
-    assert_match "0.23.0", shell_output("#{bin}/eckit-version").strip
+    assert_match version.to_s, shell_output("#{bin}/eckit-version").strip
   end
 end
