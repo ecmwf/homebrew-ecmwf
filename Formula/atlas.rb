0d7d232e819218087bb90cc3d098c2d6b8f27a2f
Dusan Figala <dusanfigala@protonmail.com>
2023-06-14 15:49:49 +0200

    atlas 0.33.0

--- Formula/atlas.rb
+++ Formula/atlas.rb
@@ -1,22 +1,30 @@
 class Atlas < Formula
-  desc "ECMWF library for parallel data-structures supporting unstructured grids and function spaces"
+  desc "ECMWF library for numerical weather prediction and climate modelling"
   homepage "https://github.com/ecmwf/atlas"
-  url "https://github.com/ecmwf/atlas/archive/0.15.2.tar.gz"
-  sha256 "8c53843be4a14111497e154087810a2a0c4a0d9941c1e3d4dd2e9189f21f4f95"
+  url "https://github.com/ecmwf/atlas/archive/refs/tags/0.33.0.tar.gz"
+  sha256 "a91fffe9cecb51c6ee8549cbc20f8279e7b1f67dd90448e6c04c1889281b0600"
 
   depends_on "cmake" => :build
   depends_on "ecbuild" => :build
   depends_on "eckit"
-  # depends_on "eigen" => :recommended # currently fails to build -- internactive make works, non-interactive fails
+  depends_on "eigen" => :recommended # currently fails to build -- internactive make works, non-interactive fails
 
   def install
     mkdir "build" do
-      system "ecbuild", "..", "-DENABLE_FORTRAN=OFF", "-DENABLE_EIGEN=OFF", *std_cmake_args
+      system "ecbuild", "..", "-DENABLE_FORTRAN=OFF", *std_cmake_args
       system "make", "install"
     end
+
+    shim_references = [
+      lib/"pkgconfig/atlas.pc",
+      lib/"pkgconfig/atlas-c++.pc",
+      include/"atlas/atlas_ecbuild_config.h",
+    ]
+    inreplace shim_references, Superenv.shims_path/ENV.cxx, ENV.cxx
+    inreplace shim_references, Superenv.shims_path/ENV.cc, ENV.cc
   end
 
   test do
-    assert_match "0.15.2", shell_output("#{bin}/atlas --version").strip
+    assert_match version.to_s, shell_output("#{bin}/atlas --version").strip
   end
 end
