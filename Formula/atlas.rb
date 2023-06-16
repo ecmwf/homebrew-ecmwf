ea32462b8d193e8f38476f3afddbfb46dd05c766
Dusan Figala <dusanfigala@protonmail.com>
2023-06-16 13:45:20 +0200

    eckit 1.24.1

--- Formula/atlas.rb
+++ Formula/atlas.rb
@@ -31,6 +31,6 @@ class Atlas < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/atlas --version").strip
+    assert_match version.to_s, shell_output("#{bin}/atlas --version")
   end
 end
